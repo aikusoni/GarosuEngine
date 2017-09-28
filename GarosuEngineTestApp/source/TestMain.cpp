4e3a7da116aa08d2d5577b44d3b7ea45a963a0d7
Loser <aikusoni@gmail.com>
2017-09-28 16:58:51 +0900

    엔진 테스트 앱 솔루션 추가

---
+++ GarosuEngineTestApp/source/TestMain.cpp
@@ -0,0 +1,11 @@
+#include <GarosuEngine.h>
+
+#include <Windows.h>
+
+using namespace Garosu;
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+
+	return 0;
+}
